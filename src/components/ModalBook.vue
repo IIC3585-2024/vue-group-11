3960c989638faebf62b503e6f8f8242d69c522bc
Pablo Carrasco <pablo.carrasco@uc.cl>
2024-05-31 04:30:36 -0400

    Se agregan botones con íconos al modal

--- src/components/ModalBook.vue
+++ src/components/ModalBook.vue
@@ -4,43 +4,48 @@
   <div v-if="open">
     <div class="vue-modal" v-show="open">
       <transition name="slide">
-                <div class="book-content">
-                <button type="button" class="back-button" @click="$emit('close')"><img src="../assets/leftarrow.png" alt="Back" class="icon-arrow"></button>
-                <button type="button" class="add-button" @click="$emit('close')"><img src="../assets/star-empty.png" alt="Back" class="icon-star"></button>
-                <div class="book" v-for="(book, index) in transformBookData" :key="index" ref="books">
-                  <template v-if="book.cover">
-                    <div class="face-front" :style="{ backgroundImage: 'url(' + book.image + ')', backgroundSize: '100% 100%' }" @click="flipCard($event, book)">
-                    </div>
-                    <div class="face-back" @click="flipCard($event, book)">
-                    </div>
-                  </template> 
-                  <template v-else>
-                    <div class="face-front" @click="flipCard($event, book)">
-                        <h1 class="inside-page-front">{{ book.title }}</h1>
-                        <p class="inside-page-front">{{ book.description }}</p>
-                        <p class="inside-page-front">{{ book.publishedDate }}</p>
-                        <p class="inside-page-front">{{ book.plubisher }}</p>
-                        <p class="inside-page-front">{{ book.contentVersion }}</p>
-                        <p class="inside-page-front">{{ book.plubisher }}</p>
-                        <p class="inside-page-front">{{ book.authors.join(', ')}}</p>
-                      </div>
-                      <div class="face-back" @click="flipCard($event, book)">
-                        {{ book.title2 }}
-                        {{ book.description2 }}
-                      </div>
-                  </template>
-                </div>
+        <div class="book-content">
+          <button type="button" class="back-button" @click="$emit('close')"><img src="../assets/x.png" alt="Back" class="icon is-medium"></button>
+          <button type="button" class="favorite-button" @click="$emit('close')"><img src="../assets/star-empty.png" alt="Favorite" class="icon is-medium"></button>
+          <div class="book" v-for="(book, index) in transformBookData" :key="index" ref="books">
+            <template v-if="book.cover">
+              <div class="face-front" :style="{ backgroundImage: 'url(' + book.image + ')', backgroundSize: '100% 100%' }" @click="flipCard($event, book)">
               </div>
-            <!-- </div> -->
-          <!-- </div> -->
-        <!-- </div> -->
+              <div class="face-back" @click="flipCard($event, book)">
+              </div>
+            </template> 
+            <template v-else>
+              <div class="face-front" @click="flipCard($event, book)">
+                <h1 class="inside-page-front">{{ book.title }}</h1>
+                <p class="inside-page-front">{{ book.description }}</p>
+                <p class="inside-page-front">{{ book.publishedDate }}</p>
+                <p class="inside-page-front">{{ book.plubisher }}</p>
+                <p class="inside-page-front">{{ book.contentVersion }}</p>
+                <p class="inside-page-front">{{ book.plubisher }}</p>
+                <p class="inside-page-front">{{ book.authors.join(', ')}}</p>
+              </div>
+              <div class="face-back" @click="flipCard($event, book)">
+                {{ book.title2 }}
+                {{ book.description2 }}
+              </div>
+            </template>
+          </div>
+          <div v-if="!bookIsInCompleted" class="completed-button">
+            <button @click="addBookToCompleted"><img src="../assets/completed.png" alt="Completed" class="icon is-large"></button>            
+          </div>
+          <div v-if="!bookIsInRecommended" class="recommended-button">
+            <button @click="addBookToRecommended"><img src="../assets/recommend.png" alt="Recommended" class="icon is-large"></button>
+          </div>
+          <div v-if="!bookIsInNext" class="next-button">
+            <button @click="addBookToNext"><img src="../assets/next.png" alt="Next" class="icon is-large"></button>
+          </div>
+        </div>
       </transition>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
 
 export default {
   props: {
@@ -55,12 +60,48 @@ export default {
       rotation: 0,
       contZindex: 2,
       customZindex: 1,
+      recommendedBooks: [],
+      completedBooks: [],
+      nextBooks: [],
+      favoriteBooks: [],
+      bookIsInRecommended: false,
+      bookIsInCompleted: false,
+      bookIsInNext: false,
+      bookIsInFavorite: false,
     };
+  },
+  mounted() {
+    if (localStorage.getItem('completedBooks')) {
+      this.completedBooks = JSON.parse(localStorage.getItem('completedBooks'));
+    }  
+
+    if (localStorage.getItem('recommendedBooks')) {
+      this.recommendedBooks = JSON.parse(localStorage.getItem('recommendedBooks'));
+    }
+
+    if (localStorage.getItem('nextBooks')) {
+      this.nextBooks = JSON.parse(localStorage.getItem('nextBooks'));
+    }
+
+    if (localStorage.getItem('favoriteBooks')) {
+      this.favoriteBooks = JSON.parse(localStorage.getItem('favoriteBooks'));
+    }
+
+    this.checkBookInLists();
+  },
+  watch: {
+    bookData: function() {
+      this.bookIsInCompleted = false;
+      this.bookIsInRecommended = false;
+      this.bookIsInNext = false;
+      this.bookIsInFavorite = false;
+      this.checkBookInLists();
+    }
   },
   computed: {
     transformBookData() {
       const bookInfo = this.bookData.volumeInfo;
-      console.log(bookInfo)
+      // console.log(bookInfo)
 
       return [
         {
@@ -129,6 +170,57 @@ export default {
         }, 500);
       }
     },
+    checkBookInLists() {
+      let searched_id = this.bookData.id;
+      // console.log(this.bookData.volumeInfo.title);
+      this.completedBooks.forEach((book) => {
+        if (book.id === searched_id) {
+          this.bookIsInCompleted = true;          
+        }        
+      })        
+
+      this.recommendedBooks.forEach((book) => {
+        if (book.id === searched_id) {
+          this.bookIsInRecommended = true;
+        }
+      })
+
+      this.nextBooks.forEach((book) => {
+        if (book.id === searched_id) {
+          this.bookIsInNext = true;
+        }
+      })
+
+      this.favoriteBooks.forEach((book) => {
+        if (book.id === searched_id) {
+          this.bookIsInFavorite = true;
+        }
+      })
+    },
+    addBookToCompleted() {
+      let currentCompletedBooks = JSON.parse(localStorage.getItem('completedBooks'));
+      currentCompletedBooks.push(this.bookData);
+      localStorage.setItem('completedBooks', JSON.stringify(currentCompletedBooks));
+      this.bookIsInCompleted = true;
+    },
+    addBookToRecommended() {
+      let currentRecommendedBooks = JSON.parse(localStorage.getItem('recommendedBooks'));
+      currentRecommendedBooks.push(this.bookData);
+      localStorage.setItem('recommendedBooks', JSON.stringify(currentRecommendedBooks));
+      this.bookIsInRecommended = true;
+    },
+    addBookToNext() {
+      let currentNextBooks = JSON.parse(localStorage.getItem('nextBooks'));
+      currentNextBooks.push(this.bookData);
+      localStorage.setItem('nextBooks', JSON.stringify(currentNextBooks));
+      this.bookIsInNext = true;
+    },
+    addBookToFavorite() {
+      let currentFavoriteBooks = JSON.parse(localStorage.getItem('favoriteBooks'));
+      currentFavoriteBooks.push(this.bookData);
+      localStorage.setItem('favoriteBooks', JSON.stringify(currentFavoriteBooks));
+      this.bookIsInFavorite = true;
+    }
   }
 };
 </script>
@@ -159,7 +251,7 @@ export default {
 
 .back-button {
   position: absolute;
-  top: 0;
+  top: 1%;
   left: 0;
   padding-left: 10px;
   background-color: #3b2b1a;
@@ -169,11 +261,11 @@ export default {
   cursor: pointer;
 }
 
-.add-button {
+.favorite-button {
   position: absolute;
-  top: 0;
+  top: 1%;
   right: 0;
-  padding: 10px;
+  padding-right: 10px;
   background-color: #3b2b1a;
   color: white;
   border: none;
@@ -181,14 +273,27 @@ export default {
   cursor: pointer;
 }
 
-.icon-arrow {
-  width: 40px;
-  height: 40px;
+.completed-button {
+  position: absolute;
+  bottom: 15px;
+  right: 80%;
+  border: none;
+  cursor: pointer;
 }
 
-.icon-star {
-  width: 30px;
-  height: 30px;
+.recommended-button {
+  position: absolute;
+  bottom: 15px;
+  right: 45%;
+  border: none;
+  cursor: pointer;
+}
+
+.next-button {
+  position: absolute;
+  bottom: 15px;
+  border: none;
+  cursor: pointer;
 }
 
 *{
@@ -210,12 +315,12 @@ h1{
 	display: flex;
 	justify-content: center;
 	align-items: center;
-  padding-top: 10px;
+  padding-top: 5px;
 }
 
 .book-content, .book-content-page{
 	width: 860px;
-	height: 485px;
+	height: 600px;
 	position: relative;
   justify-content: flex-end;
 	perspective: 1000px;
@@ -230,7 +335,7 @@ h1{
   margin: 10px;
 	position: absolute;
 	width: 275px;
-	height: 82%;
+	height: 70%;
 	transition: 1s;
 	transform-style: preserve-3d;
 	transform-origin: left; 
