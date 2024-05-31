4171e44bc8063a6cdc8ff3e98b797da81b985d68
Pablo Carrasco <pablo.carrasco@uc.cl>
2024-05-31 04:31:03 -0400

    Se agregan las nuevas rutas y las nuevas vistas

--- src/views/MyBooks.vue
+++ src/views/MyBooks.vue
@@ -1,46 +1,29 @@
 <template>
   <div>
-    <BooksList :books="booksData" :titlePage="titlePage" :buttonText="buttonText" :currentPage="currentPage" @update-page="fetchBooks" @redirectPage="redirectPage"/>
+    <BooksList :books="booksData" :titlePage="titlePage"/>
   </div>
 </template>
 
 <script>
-import BooksList from '../components/BooksList.vue';
-import axios from 'axios'
+import BooksList from '../components/NewBookList.vue';
 
 export default {
-  name: 'App',
+  name: 'CompletedBooks',
   components: {
     BooksList
   },
   data() {
     return {
       booksData: [],
-      currentPage: 0,
-      titlePage: "Mis libros",
-      buttonText: "Volver inicio",
-      pathText: "/"
+      titlePage: "Lecturas Completadas",
     };
   },
   mounted() {
-    this.fetchBooks(0);
+    if (localStorage.getItem('completedBooks')) {
+      this.booksData = JSON.parse(localStorage.getItem('completedBooks'));
+    }    
   },
   methods: {
-    fetchBooks(currentPageParam) {
-      this.currentPage = currentPageParam;
-      // mi lista de libros seleccionados
-      axios
-        .get(`https://www.googleapis.com/books/v1/volumes?q=a&startIndex=${currentPageParam * 40}&maxResults=40`)
-        .then(response => {
-          this.booksData = response.data.items;
-        })
-        .catch(error => {
-          console.error('Error fetching books:', error);
-        });
-    },
-    redirectPage() {
-      this.$router.push(this.pathText);
-    }
   }
 }
 </script>
---
+++ src/views/NextBooks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <BooksList :books="booksData" :titlePage="titlePage"/>
+  </div>
+</template>
+
+<script>
+import BooksList from '../components/NewBookList.vue';
+
+export default {
+  name: 'NextBooks',
+  components: {
+    BooksList
+  },
+  data() {
+    return {
+      booksData: [],
+      titlePage: "Lecturas Futuras",
+    };
+  },
+  mounted() {
+    if (localStorage.getItem('nextBooks')) {
+      this.booksData = JSON.parse(localStorage.getItem('nextBooks'));
+    }    
+  }
+}
+</script>
